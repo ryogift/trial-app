ecf96342e3895dc61822ca0830c38e8fc6995d7f
Ryo Takahashi <ryogiftjp@gmail.com>
2022-01-24 23:29:08 +0900

    Fix turbolinks to turbo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -52,7 +52,7 @@ class PostsController < ApplicationController
     @post.destroy
 
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+      format.html { redirect_to posts_url, notice: "Post was successfully destroyed.", status: :see_other }
       format.json { head :no_content }
     end
   end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,6 @@
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "./channels"
 import "bootstrap"
 
-Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
